dacd956f05bac52043dd8fa6d23658ef0cb06d49
Sergey Kravchenko <sergey.ose.pyatigosk@gmail.com>
2024-07-23 23:24:00 +0300

    feat: add header component

---
+++ src/components/shared/container.tsx
@@ -0,0 +1,10 @@
+import { cn } from '@/lib/utils'
+import { PropsWithChildren } from 'react'
+
+type ContainerProps = {
+  className: string
+}
+
+export const Container = ({ className, children }: PropsWithChildren & ContainerProps) => {
+  return <div className={cn('mx-auto max-w-[1280px]', className)}>{children}</div>
+}
