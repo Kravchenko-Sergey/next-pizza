a861f701fbb3d88eaef82fd6bf03702364e7595f
Sergey Kravchenko <sergey.ose.pyatigosk@gmail.com>
2024-07-27 21:10:26 +0300

    feat: change display product info

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,11 +1,34 @@
-'use client'
+import { prisma } from '@/prisma/prisma-client'
+import { notFound } from 'next/navigation'
+import { Container, ProductImage, Title } from '@/components/shared'
+import GroupVariants from '@/components/shared/group-variants'
 
-import { useParams } from 'next/navigation'
+export default async function ProductPage({ params: { id } }) {
+  const product = await prisma.product.findFirst({ where: { id: Number(id) } })
 
-const ProductPage = () => {
-  const { id } = useParams()
+  console.log(product)
 
-  return <div>{id}</div>
+  if (!product) {
+    return notFound()
+  }
+
+  return (
+    <Container className='flex flex-col my-10'>
+      <div className='flex flex-1'>
+        <ProductImage imageUrl={product.imageUrl} size={40} />
+        <div className='w-[490px] bg-[#f7f6f5] p-7'>
+          <Title text={product.name} size='md' className='font-extrabold mb-1' />
+          <p className='text-gray-400'>Lorem ipsum dolor sit amet, consectetur elit</p>
+          <GroupVariants
+            items={[
+              { name: 'Маленькая', value: '1' },
+              { name: 'Средняя', value: '2' },
+              { name: 'Большая', value: '3' }
+            ]}
+            selectedValue={'2'}
+          />
+        </div>
+      </div>
+    </Container>
+  )
 }
-
-export default ProductPage
