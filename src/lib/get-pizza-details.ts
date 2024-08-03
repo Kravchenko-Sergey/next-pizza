97193dc1a720ea3293825f6f15b3d72984ff08cc
Sergey Kravchenko <sergey.ose.pyatigosk@gmail.com>
2024-08-03 16:22:24 +0300

    fix: fix deploy

--- src/lib/get-pizza-details.ts
+++ src/lib/get-pizza-details.ts
@@ -10,7 +10,7 @@ export const getPizzaDetails = (
   selectedIngredients: Set<number>
 ) => {
   const totalPrice = getTotalPizzaPrice(type, size, items, ingredients, selectedIngredients)
-  const textDetaills = `${size} см, ${mapPizzaType[type]} пицца`
+  const textDetails = `${size} см, ${mapPizzaType[type]} пицца`
 
-  return { totalPrice, textDetaills }
+  return { totalPrice, textDetails }
 }
