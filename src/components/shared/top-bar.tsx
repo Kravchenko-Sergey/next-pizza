166c03739ec26d1f33afaee000d57848c252843e
Sergey Kravchenko <sergey.ose.pyatigosk@gmail.com>
2024-08-03 14:45:39 +0300

    add files for deploy

--- src/components/shared/top-bar.tsx
+++ src/components/shared/top-bar.tsx
@@ -10,7 +10,7 @@ type TopBarProps = {
 
 export const TopBar = ({ categories, className }: TopBarProps) => {
   return (
-    <div className={cn('sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10')}>
+    <div className={cn('sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10', className)}>
       <Container className='flex items-center justify-between'>
         <Categories categories={categories} />
         <SortPopup className='' />
