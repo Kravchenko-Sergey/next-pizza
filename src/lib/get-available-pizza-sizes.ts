459eac9603936fa0352e68e8405422d6619bf613
Sergey Kravchenko <sergey.ose.pyatigosk@gmail.com>
2024-08-03 16:54:15 +0300

    fix: fix deploy

--- src/lib/get-available-pizza-sizes.ts
+++ src/lib/get-available-pizza-sizes.ts
@@ -1,8 +1,8 @@
-import { ProductItem } from '@prisma/client'
+import { Variation } from '@prisma/client'
 import { pizzaSizes, PizzaType } from '@/constants/pizza'
 import { Variant } from '@/components/shared/group-variants'
 
-export const getAvailablePizzaSizes = (type: PizzaType, items: ProductItem[]): Variant[] => {
+export const getAvailablePizzaSizes = (type: PizzaType, items: Variation[]): Variant[] => {
   const filteredPizzasByType = items.filter(item => item.pizzaType === type)
 
   return pizzaSizes.map(item => ({
--- src/lib/get-pizza-details.ts
+++ src/lib/get-pizza-details.ts
@@ -1,11 +1,11 @@
 import { getTotalPizzaPrice } from '@/lib/calc-total-pizza-price'
-import { Ingredient, ProductItem } from '@prisma/client'
+import { Ingredient, Variation } from '@prisma/client'
 import { mapPizzaType, PizzaSize, PizzaType } from '@/constants/pizza'
 
 export const getPizzaDetails = (
   type: PizzaType,
   size: PizzaSize,
-  items: ProductItem[],
+  items: Variation[],
   ingredients: Ingredient[],
   selectedIngredients: Set<number>
 ) => {
